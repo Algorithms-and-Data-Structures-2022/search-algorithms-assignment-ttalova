38a985dce4ddc27e748c9f2ab41aa848622ae486
ttalova <talova0304@gmail.com>
2022-05-05 15:03:45 +0300

    commitik

--- src/binary_search_iterative.cpp
+++ src/binary_search_iterative.cpp
@@ -15,15 +15,16 @@ namespace assignment {
     int index_left_bord = 0;
     int index_right_bord = static_cast<int>(data.size()) - 1;
     while (index_left_bord <= index_right_bord) {
-        int middle_index = (index_left_bord + index_right_bord) / 2;
+        int middle_index = index_left_bord + ((index_left_bord - index_right_bord) / 2);
         if (search_element == data[middle_index]) {
           return middle_index;
         }
-        else if (search_element < data[middle_index]) {
-          index_right_bord = middle_index + 1;
+        if (search_element < data[middle_index]) {
+
+          index_left_bord = middle_index + 1;
         }
         else if (search_element > data[middle_index]) {
-          index_left_bord = middle_index - 1;
+          index_right_bord = middle_index - 1;
         }
       }
     return std::nullopt;
